2776f0fb0731c9bcf2aedf9acff5514cca02eae4
exalthab <exalthab@gmail.com>
2025-04-26 19:40:53 +0000

    Cleaned up large files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./index.html"],  // Tell Tailwind where are the classes
+    theme: {
+      extend: {
+        colors:{
+            brand:"", // Custom color for the header/brand
+        }
+      }
+    },
+    plugins: [],
+  };
+  
